8edaf7e859f341550d42804b46e5b7d7387a351e
akmalranyan <akmalranyan@gmail.com>
2023-02-12 14:15:00 +0700

    fixed signup view

--- resources/views/auth/signup-adopter.blade.php
+++ resources/views/auth/signup-adopter.blade.php
@@ -24,11 +24,11 @@
             <i class="fa-solid fa-paw fa-xl mt-[15px]" style="color: white;"></i>
             <p class=" text-2xl text-white">Doppie</p>
         </div>
-        <div class="bg-slate-700 h-screen flex justify-center">
+        <div class="bg-slate-700 h-[90vh] flex justify-center">
 
-            <div class="card bg-slate-900 mt-20 h-[500px] p-8 w-[500px] rounded-lg">
+            <div class="card bg-slate-900 mt-20 h-[600px] p-8 w-[500px] rounded-lg">
                 <div class="card-body">
-                    <form action="{{ route('signup-adopter') }}" method="POST">
+                    <form action="{{ route('register') }}" method="POST">
                         @csrf
                         <h2 class="text-slate-200 text-2xl">Sign Up</h2>
                         <div class="">
@@ -68,6 +68,10 @@
                                 {{ $message }}
                             </span>
                             @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mb-2 mt-2">Confirm Password</label>
+                            <input name="password_confirmation" type="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         </div>
 
                         <div class="">
